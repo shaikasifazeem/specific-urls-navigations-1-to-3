b1eb665ce005096a3fc84e218fbfa041964daa5b
shaikasifazeem <asifazeem520@gmail.com>
2023-03-17 00:16:36 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+def asif(request):
+    return render(request,'asif.html')
+
+def azeem(request):
+    return render(request,'azeem.html')
+
+# Create your views here.
---
+++ app2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+def jsp(request):
+    return render(request,'jsp.html')
+
+def spvm(request):
+    return render(request,'spvm.html')
+    
+
+# Create your views here.
